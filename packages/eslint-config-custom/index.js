fbc3bfcb90ad76a90b2332f4c967c00e775a2343
Stuard Gerardo Carrillo Gonzalez <stuard@supergroup.co>
2023-03-18 17:58:26 -0600

    eslint setup

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,6 +1,14 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  extends: [
+    "next/core-web-vitals",
+    "plugin:@next/next/recommended",
+    "turbo",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
   rules: {
     "@next/next/no-html-link-for-pages": "off",
+    "prettier/prettier": "error",
   },
 };
