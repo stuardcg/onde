efcdb41984ded211d9c696872034db1a7ac6560c
Stuard Gerardo Carrillo Gonzalez <stuard@supergroup.co>
2023-03-18 19:43:21 -0600

    add tailwind

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,10 +1,22 @@
+import "../styles.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
+    <ClerkProvider
+      {...pageProps}
+      appearance={{ variables: { fontFamily: inter.style.fontFamily } }}
+    >
+      <main className={`${inter.variable} font-sans`}>
+        <Component {...pageProps} />
+      </main>
     </ClerkProvider>
   );
 }
---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-inter)", ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
