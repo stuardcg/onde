a726822879dcaeb3427a3b38f88c665202462129
Stuard Gerardo Carrillo Gonzalez <stuard@supergroup.co>
2023-03-18 19:05:58 -0600

    add clerk

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,15 @@
-import { Button } from "ui";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
 
-export default function Web() {
+export default function Index() {
   return (
     <div>
-      <h1>Web</h1>
-      <Button />
+      <SignedIn>
+        Welcome <UserButton />
+      </SignedIn>
+      <SignedOut>
+        <Link href="/sign-up">Sign up</Link>
+      </SignedOut>
     </div>
   );
 }
